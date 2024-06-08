7b21a2642a556f09a20505e4f4f73264dec08af4
adhityabye <mr.adhityabayu@gmail.com>
2024-06-09 04:29:15 +0700

    initial project

---
+++ gateway/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const proxy = require("express-http-proxy");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/customer", proxy("http://localhost:3001"));
+app.use("/shop", proxy("http://localhost:3003"));
+app.use("/", proxy("http://localhost:3002")); //books
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+  console.log("Hello");
+});
