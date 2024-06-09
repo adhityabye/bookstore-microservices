bb7c5237c5400f3de8ef1a5a420549e6bb980554
adhityabye <mr.adhityabayu@gmail.com>
2024-06-09 22:14:27 +0700

    feat(customer): db connection, models, and respository

---
+++ customer/src/config/index.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv");
+
+if (process.env.NODE_ENV !== "prod") {
+  const configFile = `./.env.${process.env.NODE_ENV}`;
+  dotenv.config({ path: configFile });
+} else {
+  dotenv.config();
+}
+
+console.log("APP_SECRET:", process.env.APP_SECRET);
+
+module.exports = {
+  PORT: process.env.PORT,
+  DB_URL: process.env.MONGODB_URI,
+  APP_SECRET: process.env.APP_SECRET,
+};
